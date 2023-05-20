e5fbffbe7d6306d0271535d01faefcdcce968774
Tufan AKÇAY <132299647+tufanakcay@users.noreply.github.com>
2023-05-20 21:41:46 +0300

    Update MainActivity.kt

--- app/src/main/java/com/tufanakcay/kotlincountdown/MainActivity.kt
+++ app/src/main/java/com/tufanakcay/kotlincountdown/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.tufanakcay.kotlincountdown
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.os.CountDownTimer
+import com.tufanakcay.kotlincountdown.databinding.ActivityMainBinding
+@SuppressLint("SetTextI18n")
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+
+        //Abstract - Inheritance
+
+
+        object : CountDownTimer(10000,1000) {
+            override fun onFinish() {
+                binding.textView.text = "Left: 0"
+            }
+
+
+            override fun onTick(millisUntilFinished: Long) {
+                binding.textView.text = "Left: ${millisUntilFinished/1000}"
+            }
+
+        }.start()
+
+
     }
 }
